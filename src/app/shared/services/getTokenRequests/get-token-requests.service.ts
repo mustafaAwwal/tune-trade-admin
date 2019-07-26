a961b98230ef697dd4db016fcbedc2d3e236ec24
Mustafa Awwal <mustafaawwal1@gmail.com>
2019-07-26 17:26:50 +0500

    new services added

--- src/app/shared/services/getTokenRequests/get-token-requests.service.ts
+++ src/app/shared/services/getTokenRequests/get-token-requests.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment_url } from 'src/environments/environment';
-import { GetAllTokensReponseModels, insertTokenFromListModel } from 'src/app/shared/models';
+import { GetAllTokensReponseModels, insertTokenFromListModel, RemoveLiveTokenModel } from 'src/app/shared/models';
 import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
@@ -13,25 +13,35 @@ export class GetTokenRequestsService {
     getAlltokens: environment_url + 'getalltokens',
     getListTokens: environment_url + 'getListTokens',
     insertTokenData: environment_url + 'inserttokendata',
-    getTokenData: environment_url + 'gettokendata'
+    getTokenData: environment_url + 'gettokendata',
+    removeLiveToken: environment_url + 'removeToken',
+    getDeployTokens: environment_url + 'getdeploytokens'
   };
   constructor(private http: HttpClient, private router: Router) { }
   getAllTokens(): Observable<HttpResponse<any>> {
     const httpOptions = this.getHeaders();
-    return this.refreshToken(this.http.get<HttpResponse<GetAllTokensReponseModels>>(this.urls.getAlltokens, httpOptions));
+    return this.http.get<HttpResponse<GetAllTokensReponseModels>>(this.urls.getAlltokens, httpOptions);
+  }
+  removeToken(body: RemoveLiveTokenModel): Observable<HttpResponse<any>> {
+    const httpOptions = this.getHeaders();
+    return this.http.post<HttpResponse<any>>(this.urls.removeLiveToken, body, httpOptions);
   }
   getListTokens(): Observable<HttpResponse<any>> {
     const httpOptions = this.getHeaders();
-    return this.refreshToken(this.http.get<HttpResponse<GetAllTokensReponseModels>>(this.urls.getListTokens, httpOptions));
+    return this.http.get<HttpResponse<GetAllTokensReponseModels>>(this.urls.getListTokens, httpOptions);
   }
   insertTokenFromList(tokenData: insertTokenFromListModel): Observable<HttpResponse<any>> {
     const httpOptions = this.getHeaders();
-    return this.refreshToken(this.http.post<HttpResponse<any>>(this.urls.insertTokenData, tokenData, httpOptions));
+    return this.http.post<HttpResponse<any>>(this.urls.insertTokenData, tokenData, httpOptions);
 
   }
   getTokenData(): Observable<HttpResponse<any>> {
     const httpOptions = this.getHeaders();
-    return this.refreshToken(this.http.get<HttpResponse<any>>(this.urls.getTokenData, httpOptions));
+    return this.http.get<HttpResponse<any>>(this.urls.getTokenData, httpOptions);
+  }
+  getDeployTokens() {
+    const httpOptions = this.getHeaders();
+    return this.http.get<HttpResponse<any>>(this.urls.getDeployTokens, httpOptions);
   }
   getHeaders() {
     const httpOptions = {
@@ -42,17 +52,19 @@ export class GetTokenRequestsService {
     };
     return httpOptions;
   }
-  refreshToken(response: Observable<HttpResponse<any>>) {
-    response.subscribe(res => {
-      const headerValue = res.headers.get('token');
-      const status      = res.body['status'];
-      if (status) {
-        sessionStorage.setItem('token', headerValue);
-      } else {
-        // sessionStorage.clear();
-        // this.router.navigate(['']);
-      }
-    });
-    return response;
-  }
+  // refreshToken(response: Observable<HttpResponse<any>>) {
+  //   response.subscribe(res => {
+  //     console.log('in subscription')
+  //     console.log(res);
+  //     const headerValue = res.headers.get('token');
+  //     const status      = res.body['status'];
+  //     if (status) {
+  //       sessionStorage.setItem('token', headerValue);
+  //     } else {
+  //       // sessionStorage.clear();
+  //       // this.router.navigate(['']);
+  //     }
+  //   });
+  //   return response;
+  // }
 }
