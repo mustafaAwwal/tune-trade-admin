c340a39b2bca495385f38499893302aad5ec8bb6
Mustafa Awwal <mustafaawwal1@gmail.com>
2019-07-29 16:42:09 +0500

    Solc and web 3 installed

--- src/app/modules/admin/admin-home/create-token-table/admin-token-view/admin-token-view.component.ts
+++ src/app/modules/admin/admin-home/create-token-table/admin-token-view/admin-token-view.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, NgZone, Input, Output, EventEmitter } from '@angular/core';
 import { from } from 'rxjs';
-// import Web3 from 'web3';
-// const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8546', null, {});
+import Web3 from 'web3';
+import solc from 'solc';
+const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8546', null, {});
 
 @Component({
   selector: 'app-admin-token-view',
@@ -15,43 +16,41 @@ export class AdminTokenViewComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log(this.token);
-    // this.grantAccess().then(() => this.getAccounts());
+    this.grantAccess().then(() => this.getAccounts());
   }
 
-//  getAccounts() {
-//     this.zone.run(() => {
-//       from(web3.eth.getAccounts()).subscribe(res => {
-//         console.log(res);
-//         console.log(web3.eth.net.isListening().then(console.log));
-//       });
-//     }
-//     ); 
-//   }
+ getAccounts() {
+    this.zone.run(() => {
+      from(web3.eth.getAccounts()).subscribe(res => {
+        console.log(res);
+        console.log(web3.eth.net.isListening().then(console.log(true)));
+      });
+    }
+    ); 
+  }
   closeView() {
     this.closingTrigger.emit(true);
   }
-  // grantAccess() {
-  //   let promise = new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       if ((window as any)[ 'ethereum' ]) {
-  //         console.log('inPromise');
-  //         try {
-  //             // Request account access if needed
-  //             if ((window as any)['ethereum'].enable()) {
-  //               resolve();
-  //             }
-  //         } catch (error) {
-  //           reject();
-  //         }
-  //         console.log('calling resolve')
-  //     }
-  //     }, 1000);
-  //   });
-  //   return promise;
-  // }
+  grantAccess() {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if ((window as any)[ 'ethereum' ]) {
+          console.log('inPromise');
+          try {
+              // Request account access if needed
+              if ((window as any)['ethereum'].enable()) {
+                resolve();
+              }
+          } catch (error) {
+            reject();
+          }
+          console.log('calling resolve')
+      }
+      }, 1000);
+    });
+    return promise;
+  }
   deployToken() {
-    
     console.log(this.token);
   }
 }
